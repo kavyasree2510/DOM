3b9559a809d8a76a4cf8ab02a6df5401dc84e6f2
kavyasree2510 <kavyareddie2510@gmail.com>
2025-06-25 15:18:54 +0530

    dom

---
+++ dome.js
@@ -0,0 +1,8 @@
+document.title="DOM in JavaScript";
+document.body.style.backgroundColor= "lightblue";
+document.getElementById("header").innerHTML="HELLO WORLD!";
+document.getElementById("para").innerHTML="Welcome to DOM Manipulation";
+document.getElementsByTagName("h1")[1].style.color="red";
+document.getElementsByClassName("DS")[0].style.color="green";
+document.getElementsByClassName("DS")[1].style.color="blue";
+document.getElementsByClassName("DS")[2].style.color="brown";
